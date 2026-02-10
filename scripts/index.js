626b09e5b0fc1f8e2c123e98f62a7049ac752961
Krittameth Tansuwan <99584671+GERRYExpress@users.noreply.github.com>
2026-02-10 18:31:39 +0700

    Development (#22)
    * add first column structure
    
    * add all content structure
    
    * Add all of the crucial content
    
    * setup portfolio page
    
    * add portfolio image to images folder
    
    * add carousel with portfolio short description
    
    * update CDN Bootstrap to version 5.3.8
    
    * add back button in portfolio.html, and change styling the button in index.html
    
    * change the page redirection by using relative instead of absolute path
    
    * add skill scale bar
    
    * Hotfix0.2.2 (#16)
    
    * Merging development branch to main (#9)
    
    * add first column structure
    
    * add all content structure
    
    * Add all of the crucial content
    
    * setup portfolio page
    
    * add portfolio image to images folder
    
    * add carousel with portfolio short description
    
    * update CDN Bootstrap to version 5.3.8
    
    * add back button in portfolio.html, and change styling the button in index.html
    
    * change the page redirection by using relative instead of absolute path
    
    * change data-{attr} to data-bs-{attr} for matching with BS v5
    
    * making the page responsive on sm, md, lg device
    
    * Add new sections on navbar and add event handlings
    
    * add contact section
    
    * add link to contacts
    
    * Feature/event handling (#21)
    
    * Add new sections on navbar and add event handlings
    
    * add contact section
    
    * add link to contacts
    
    * update navbar

---
+++ scripts/index.js
@@ -0,0 +1,75 @@
+$("#to-experience").on("click", () => {
+    $("#experience").css("background-color", "#011433");
+    $("#experience").css("color", "#E9EDF5");
+    $("#experience").css("padding", "25px 15px");
+    setTimeout(()=> {
+        $("#experience").css("background", "none");
+        $("#experience").css("color", "#011433");
+        $("#experience").css("padding", "0");
+    }, 1000);
+});
+
+$("#to-education").on("click", () => {
+    $("#education").css("background-color", "#011433");
+    $("#education").css("color", "#E9EDF5");
+    $("#education").css("padding", "25px 15px");
+    setTimeout(()=> {
+        $("#education").css("background", "none");
+        $("#education").css("color", "#011433");
+        $("#education").css("padding", "0");
+    }, 1000);
+});
+
+$("#to-skill").on("click", () => {
+    $("#skill").css("background-color", "#011433");
+    $("#skill").css("color", "#E9EDF5");
+    $("#skill").css("padding", "25px 15px");
+    setTimeout(()=> {
+        $("#skill").css("background", "none");
+        $("#skill").css("color", "#011433");
+        $("#skill").css("padding", "0");
+    }, 1000);
+});
+
+$("#to-hobby").on("click", () => {
+    $("#hobby").css("background-color", "#011433");
+    $("#hobby").css("color", "#E9EDF5");
+    $("#hobby").css("padding", "25px 15px");
+    setTimeout(()=> {
+        $("#hobby").css("background", "none");
+        $("#hobby").css("color", "#011433");
+        $("#hobby").css("padding", "0");
+    }, 1000);
+});
+
+$("#to-contact").on("click", () => {
+    $("#contact").css("background-color", "#011433");
+    $("#contact > p").css("color", "#E9EDF5");
+    $("#contact").css("padding", "25px 15px");
+    setTimeout(()=> {
+        $("#contact").css("background", "none");
+        $("#contact > p").css("color", "#011433");
+        $("#contact").css("padding", "0");
+    }, 1000);
+});
+
+$("#to-portfolio").on("click", () => {
+    $("#portfolio").css("background-color", "#011433");
+    $("#portfolio").css("color", "#E9EDF5");
+    $("#portfolio").css("padding", "25px 15px");
+    setTimeout(()=> {
+        $("#portfolio").css("background", "none");
+        $("#portfolio").css("color", "#011433");
+        $("#portfolio").css("padding", "0");
+    }, 1000);
+});
+
+
+$("#portfolio-button").on("click", () => {
+    document.body.classList.add("loading");
+    $("#portfolio-button").text("Loading...");
+    $("#portfolio-button").attr("disabled", "true")
+    setTimeout(() => {
+        window.location.replace("./pages/portfolio.html")
+    }, 1000);
+});
